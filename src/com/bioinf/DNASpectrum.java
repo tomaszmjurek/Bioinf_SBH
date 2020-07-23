cd6f033b0d7f2cc7684a78ca135b8e5f6f81724c
jurekt <tomasz.jurek@snpgroup.com>
2020-07-23 16:47:28 +0200

    add negative and positive errors

--- src/com/bioinf/DNASpectrum.java
+++ src/com/bioinf/DNASpectrum.java
@@ -1,20 +1,23 @@
 package com.bioinf;
 
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class DNASpectrum {
 
 	private String dna;
 	public static String firstOligo;
 	private Map<String, Integer> oligosMap = new HashMap<>();
-
-	public Map<String, Integer> getOligosMap() {
-		return oligosMap;
-	}
+	private List<String> oligosList = new ArrayList<>();
+	private ThreadLocalRandom threadLocalRandom = ThreadLocalRandom.current();
 
 	public DNASpectrum(String DNA) {
 		this.dna = DNA;
 		firstOligo = DNA.substring(0, Main.OLIGOS_SIZE);
+	}
+
+	public Map<String, Integer> getOligosMap() {
+		return oligosMap;
 	}
 
 	/**
@@ -26,6 +29,7 @@ public class DNASpectrum {
 			String oligo = dna.substring(i, i + Main.OLIGOS_SIZE);
 			oligosMap.merge(oligo, 1, Integer::sum);
 		}
+		createOligosList();
 	}
 
 	public String getDna() {
@@ -40,8 +44,52 @@ public class DNASpectrum {
 		oligosMap.forEach((key, value) -> System.out.println(key + " " + value));
 	}
 
-	public void generatePositiveErrors() {
-		
+	/**
+	 * Add negative errors to simulate real life experiment.
+	 * Some oligonucleotides from original DNA where lost.
+	 * If oligo count was 1 it is removed from map.
+	 */
+	public void generateNegativeErrors() {
+		int toDeleteCount = oligosMap.size() / Main.ERRORS_STEP;
+		for (int i = 0; i < toDeleteCount; i++) {
+			int toDeleteIndex = threadLocalRandom.nextInt(oligosMap.size());
+			String oligo =  oligosList.get(toDeleteIndex);
+			if /* it's first oligo */ (oligo.equals(firstOligo)) i--;
+			else {
+				if (oligosMap.get(oligo) == 1) oligosMap.remove(oligo);
+				else oligosMap.replace(oligo, oligosMap.get(oligo) - 1); // newValue?
+				System.out.println("Negative error occurred - removed one " + oligo);
+			}
+		}
 	}
 
+	/**
+	 * Add negative errors to simulate real life experiment.
+	 * We got some extra oligos not occurring in original DNA.
+	 * If oligo already exists it's count is incremented.
+	 */
+	public void generatePositiveErrors() {
+		int toAddCount = oligosMap.size() / Main.ERRORS_STEP;
+		for (int i = 0; i < toAddCount; i++) {
+			String randomOligo = generateRandomOligo();
+			if  (oligosMap.containsKey(randomOligo))
+				oligosMap.replace(randomOligo, oligosMap.get(randomOligo) + 1); // newValue?
+			else oligosMap.put(randomOligo, 1);
+			System.out.println("Positive error occurred - added one " + randomOligo);
+		}
+	}
+
+	private void createOligosList() {
+		for (Map.Entry<String, Integer> entry : oligosMap.entrySet())
+			oligosList.add(entry.getKey());
+	}
+
+	private String generateRandomOligo() {
+		StringBuilder oligoBuilder = new StringBuilder();
+		for (int i = 0; i < Main.OLIGOS_SIZE; i++) {
+			char randomNucleotide = Main.NUCLEOTIDES[threadLocalRandom.nextInt(4)];
+			oligoBuilder.append(randomNucleotide);
+		}
+		return oligoBuilder.toString();
+	}
 }
--- src/com/bioinf/Main.java
+++ src/com/bioinf/Main.java
@@ -7,10 +7,11 @@ public class Main {
 
     public static int OLIGOS_SIZE = 3;
     public static int DNA_SIZE;
-    public static int POPULATION_SIZE = 500;
-    public static int GENERATIONS_NUMBER = 200;
+    public static int POPULATION_SIZE = 100; //500
+    public static int GENERATIONS_NUMBER = 100; //200
     public static double CROSSOVER_PROBABILITY = 95; // %
     public static double MUTATION_PROBABILITY = 4; // %
+    public static int ERRORS_STEP = 5; // error for each step
     public static char [] NUCLEOTIDES = { 'A', 'T', 'C', 'G'};
 
     public static void main(String[] args) {
@@ -19,16 +20,16 @@ public class Main {
         String DNA2 = "ATGATGATC"; //ATGx2 TGAx2 GATx2 ATC
         String DNA3 = "TTCGCATGACTGCATGCTGACTAGCTACGTATATTACGCGGCGCTAGCCCATGCACGCTAGTACGTCGTACGT";
 
-        DNA_SIZE = DNA3.length();
-        DNASpectrum spectrum = new DNASpectrum(DNA3);
-
+        DNA_SIZE = DNA1.length();
+        DNASpectrum spectrum = new DNASpectrum(DNA1);
         System.out.println("Spectrum: " + spectrum.getDna());
-        // bledy pozytywne - dodajemy pare nieistniejacych wczesniej oligo o licznosci 1
-        // bledy negatywne - usuwamy pare oligo (zmniejszamy licznosc czy tylko te z 1?)
-        spectrum.cutDNAIntoOligos();
 
-        System.out.println("\nOligonucleotides:");
+        spectrum.cutDNAIntoOligos();
+        System.out.println("\nOligonucleotides without errors:");
         spectrum.printOligos();
+
+        spectrum.generateNegativeErrors();
+        spectrum.generatePositiveErrors();
 
         Graph graph = new Graph();
         graph.buildGraph(spectrum.getOligosMap());
@@ -38,5 +39,7 @@ public class Main {
 
         GeneticAlgorithm algorithm = new GeneticAlgorithm(spectrum.getOligosMap(), graph);
         algorithm.run();
+
+        System.out.println("\nOriginal DNA was " + spectrum.getDna());
     }
 }
